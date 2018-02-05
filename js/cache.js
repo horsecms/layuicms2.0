7c608db0d84916473855cb7d333c208cef796294
Admin <mahm@hjb365.com>
2018-02-05 10:48:02 +0800

    优化提示

--- js/cache.js
+++ js/cache.js
@@ -7,7 +7,7 @@ layui.use(['form','jquery',"layer"],function() {
 
     //判断是否web端打开
     if(!/http(s*):\/\//.test(location.href)){
-        layer.alert("请先将项目部署到 localhost 下再进行访问，否则部分数据将无法显示");
+        layer.alert("请先将项目部署到 localhost 下再进行访问【建议通过tomcat、webstorm、hb等方式运行，不建议通过iis方式运行】，否则部分数据将无法显示");
     }else{    //判断是否处于锁屏状态【如果关闭以后则未关闭浏览器之前不再显示】
         if(window.sessionStorage.getItem("lockcms") != "true" && window.sessionStorage.getItem("showNotice") != "true"){
             showNotice();
